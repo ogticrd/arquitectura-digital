1dcc00e248146c0b329c8bd43c8918c69579199f
Tomas Familia <tomas.familia@outlookcom>
2024-06-06 09:21:15 -0400

    added more comments to main.dart

--- docs/projects/cuc/examples/flutter/lib/main.dart
+++ docs/projects/cuc/examples/flutter/lib/main.dart
@@ -1,7 +1,5 @@
 import 'dart:async';
 import 'dart:convert';
-import 'dart:developer';
-import 'dart:io';
 import 'package:http/http.dart' as http;
 import 'package:app_links/app_links.dart';
 import 'package:flutter/material.dart';
@@ -33,9 +31,7 @@ Future<oauth2.Client> createClient() async {
   // owner has authorized, they'll be redirected to `redirectUrl` with an
   // authorization code. The `redirect` should cause the browser to redirect to
   // another URL which should also have a listener.
-  //
-  // `redirect` and `listen` are not shown implemented here. See below for the
-  // details.
+
   await redirect(authorizationUrl);
   var responseUrl = await listen(redirectUrl);
 
@@ -96,23 +92,23 @@ class _MyAppState extends State<MyApp> {
               ElevatedButton(
                   onPressed: () async {
                     var client = await createClient();
-                    inspect(client);
-                    inspect(client.credentials);
 
                     var headers = {
                       'Authorization': 'Bearer ${client.credentials.accessToken}',
                     };
-
+                    //Get user info by doing an HTTP GET request
                     final response = await http.get(Uri.parse('https://cuenta.digital.gob.do/userinfo'),
                         headers: headers);
 
                     if (response.statusCode == 200) {
+                      //Print the response in case it was succesfully retrieved
                       print(response.body);
                       setState(() {
                         name = jsonDecode(response.body)["given_name"];
                       });
                     }
                     else {
+                      //Print the reason phrase in case the request failed
                       print(response.reasonPhrase);
                     }
 
